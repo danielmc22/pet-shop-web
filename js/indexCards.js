0a5e45ab56909a2d2b6f823df7cca6da6a4b40ce
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-02-13 01:24:51 -0300

    Terminado renderizado cards

--- js/indexCards.js
+++ js/indexCards.js
@@ -4,6 +4,10 @@ let productos;
 let juguetes = [];
 let medicamentos = [];
 
+const cardsContainer = document.querySelector('.product-card-container');
+
+console.log(cardsContainer);
+
 const fetchData = async (url) => {
 
     await fetch(url)
@@ -27,12 +31,32 @@ const fetchData = async (url) => {
     console.table(juguetes);
     console.table(medicamentos);
 
+    displayCards(productos);
+
 }
 
-const displayCards = () => {
+const displayCards = (dataProducto) => {
 
-    
+    let templateHTML = '';
 
+    dataProducto.map(producto => {
+
+        templateHTML += `
+        <div class="card">
+            <figure class="card-image-container">
+                <img class="card__image" src="${producto.imagen}" alt="producto">
+            </figure>
+            <h3>Nombre producto</h3>
+            <p>Precio</p>
+            <p>Cantidad:</p>
+            <input type="number">
+            <button src="#">Agregar al carrito</button>
+        </div>
+        `
+
+    });
+
+    cardsContainer.innerHTML = templateHTML;
 }
 
 fetchData(API);
